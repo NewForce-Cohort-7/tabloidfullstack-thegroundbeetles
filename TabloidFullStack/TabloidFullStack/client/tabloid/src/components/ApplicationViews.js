a9bfd7f6fe23a59e83b0aa4b746b417281617ee6
Christian Moore <christianmoore672@gmail.com>
2023-07-19 14:52:05 -0400

    new main

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
+import ViewTags from "./Tags/ViewTags";
 
 export default function ApplicationViews() {
 
  return(
       <Routes>
+        
         <Route path="/" element={<Hello />} />
+
+        <Route path="/tags" element={<ViewTags />} >
+        </Route>
+
       </Routes>
+
+      
    );
  
 }
