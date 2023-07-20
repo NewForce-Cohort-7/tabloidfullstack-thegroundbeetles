26e35f5f72fb08ff29a81a4e2ee4061f90d960e8
Marquise Campbell <marquisetyrrell@gmail.com>
2023-07-20 14:31:57 -0400

    orderby

--- TabloidFullStack/TabloidFullStack/Repositories/PostRepository.cs
+++ TabloidFullStack/TabloidFullStack/Repositories/PostRepository.cs
@@ -31,7 +31,8 @@ namespace TabloidFullStack.Repositories
                               LEFT JOIN Category c ON p.CategoryId = c.id
                               LEFT JOIN UserProfile u ON p.UserProfileId = u.Id
                               LEFT JOIN UserType ut ON u.UserTypeId = ut.id
-                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()";
+                        WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()
+                            ORDER BY PublishDateTime desc";
                     var reader = cmd.ExecuteReader();
 
                     var posts = new List<Post>();
