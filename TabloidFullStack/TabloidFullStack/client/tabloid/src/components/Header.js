9cc7cc6f98da0a39225550e6b4ef1f5ad00302f5
Marquise Campbell <marquisetyrrell@gmail.com>
2023-07-20 10:18:11 -0400

    postLink

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Header.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Header.js
@@ -26,6 +26,7 @@ export default function Header({isLoggedIn, setIsLoggedIn}) {
             {isLoggedIn &&
               <NavItem>
                 <NavLink tag={RRNavLink} to="/">Home</NavLink>
+                <NavLink tag={RRNavLink} to="/posts">Posts</NavLink>
               </NavItem>
             }
           </Nav>
