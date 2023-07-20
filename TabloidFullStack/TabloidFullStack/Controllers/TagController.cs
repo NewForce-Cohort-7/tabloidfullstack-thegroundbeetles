7f73e2a4e9f70b2835a977f0decea844dc1aa358
Christian Moore <christianmoore672@gmail.com>
2023-07-20 15:37:19 -0400

    new main

--- TabloidFullStack/TabloidFullStack/Controllers/TagController.cs
+++ TabloidFullStack/TabloidFullStack/Controllers/TagController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TabloidFullStack.Repositories;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +9,17 @@ namespace TabloidFullStack.Controllers
     [ApiController]
     public class TagController : ControllerBase
     {
+        private readonly ITagRepository _tagRepository;
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
         // GET: api/<ValuesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_tagRepository.GetAllTags());
         }
 
-        // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ValuesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
