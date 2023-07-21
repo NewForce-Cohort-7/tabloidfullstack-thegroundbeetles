1d2a30c5a920d5640b083b8e15f07d26fe75e206
Amanda Wright <akhawkins1@hotmail.com>
2023-07-21 10:41:12 -0400

    new main

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
 import CommentList from "./CommentList";
 import CategoryList from "./CategoryList";
+import CategoryForm from "./CategoryForm";
 import { Post } from "./Post/Post";
 import { PostList } from "./Post/PostList";
 
@@ -15,6 +16,7 @@ export default function ApplicationViews() {
           {/* route for the commentlist uses :postId ->using url parameter */}
       <Route path="/comments/:postId" element={<CommentList />} />
       <Route path="/category" element= {<CategoryList />} />
+      <Route path="/posts/add" element={<CategoryForm />} />
       <Route path="/posts" element= {<PostList />} />
       </Routes>
    );
